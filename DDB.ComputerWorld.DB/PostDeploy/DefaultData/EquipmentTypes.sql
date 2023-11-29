12d3ef087ef1c403c970c0626d6bcd92e6c65e09
Dean Bartel <300089145@fvtc.edu>
2023-11-29 16:59:40 -0600

    watched video 8

---
+++ DDB.ComputerWorld.DB/PostDeploy/DefaultData/EquipmentTypes.sql
@@ -0,0 +1,15 @@
+﻿begin
+	insert into dbo.tblEquipmentType (Id, Name)
+	values 
+	(0,'Server'),
+    (1,'Desktop'),
+    (2,'Laptop'),
+    (3,'Printer'),
+    (4,'Tablet'),
+    (5,'Office Chair'),
+    (6,'Lawn Chair'),
+    (7,'Light Bulb'),
+    (8,'Widget')
+end
+
+        
